9b2b42473761402e4dcdb1affce625138a4c7597
root <root@DESKTOP-9Q9L0F2.localdomain>
2026-08-14 14:02:01 -0300

    adicionei um print

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ import MYTOOLS as mt
 print(f"Número pi com 4 casas decimais: {mt.pi_real(4)}")
 
 print(f"Número neperiano com 7 casas decimais: {mt.e_real(7)}")
+
+print(f"Número pi com 6 casas decimais: {mt.pi_real(6)}")
